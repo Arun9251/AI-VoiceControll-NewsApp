5af15851a6837cfebddfaf5978bbe19f7eb522a3
Arun Singh <77932309+Arun9251@users.noreply.github.com>
2021-04-13 02:11:16 +0530

    Updates

---
+++ precache-manifest.4bab8b656ab1532cca87a7536034ede2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5701cc18811931eb3f29ef8f8754006e",
+    "url": "/AI-VoiceControll-NewsApp/index.html"
+  },
+  {
+    "revision": "af75f925efd5b3b66f2a",
+    "url": "/AI-VoiceControll-NewsApp/static/css/main.63f73e6c.chunk.css"
+  },
+  {
+    "revision": "c016db7acf5b27c35108",
+    "url": "/AI-VoiceControll-NewsApp/static/js/2.1bfcf88b.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/AI-VoiceControll-NewsApp/static/js/2.1bfcf88b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af75f925efd5b3b66f2a",
+    "url": "/AI-VoiceControll-NewsApp/static/js/main.531faad7.chunk.js"
+  },
+  {
+    "revision": "4db4dbeb36e8114d3d1b",
+    "url": "/AI-VoiceControll-NewsApp/static/js/runtime-main.59a0479f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/AI-VoiceControll-NewsApp/precache-manifest.4bab8b656ab1532cca87a7536034ede2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AI-VoiceControll-NewsApp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
